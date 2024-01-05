a70a69abd10562fe71e79be322df0a158b973191
nitzmali <nitsmali@hotmail.com>
2024-01-05 18:30:23 -0500

    job_transition_code_commit

--- pages/projects_page.py
+++ pages/projects_page.py
@@ -109,7 +109,7 @@ class ProjectPage:
             },
             {
                 "title": "Blogging Website",
-                "image": "https://github.com/nitzmali/portfolio/blob/main/assets/images/Rutgers_logo_2.jpg.png?raw=true",
+                "image": "https://github.com/nitzmali/portfolio/blob/main/assets/images/Rutgers_logo_2.jpg?raw=true",
                 "description": "Digital Innovations: Django-Powered Blogging for Rutger's Students",
                 "details":'I have successfully developed and launched two distinct web applications, showcasing versatility and technical prowess in modern web development. The first is a dynamic blogging website built using the Django framework, a testament to my skills in Python and full-stack development. This platform offers an intuitive user interface and robust features, catering to a wide range of blogging needs and published on Heroku for seamless accessibility.',
                 "insights": "Insights from Project 1",
@@ -145,7 +145,7 @@ class ProjectPage:
             },
                     {
                 "title": "Airbnb Travel",
-                "image": '<iframe src="https://slides.com/nitinmali/capstone/embed" width="900" height="420" title="Capstone Presentation" scrolling="no" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
+                "image": '<iframe src="https://slides.com/nitinmali/capstone/embed" width="830" height="391" title="Capstone Presentation" scrolling="no" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
                 "description": "Innovating Travel (Airbnb) : From Predictive Analytics to Automated Airport Navigation",
                 "details":"Embarking on a mission to redefine the travel experience, my projects intertwine the power of predictive analytics with smart automation. The first initiative leverages Random Forest algorithms to accurately predict a guest's next travel destination, enhancing the Airbnb booking experience. By analyzing patterns and preferences, this system intelligently forecasts where travelers are most likely to book their next journey, bringing a new level of personalization to the travel industry.</p><p>The second venture focuses on streamlining the airport experience. We've developed a web application tailored for airport lounges, designed to automate room bookings and guide passengers efficiently to their designated gates. This system not only simplifies the booking process but also employs advanced algorithms to find the shortest path to the assigned gate, ensuring a swift and stress-free transition for travelers. Our goal is to transform waiting times into moments of comfort, offering a seamless, user-friendly service that caters to the modern traveler's needs.",
                 "insights": "Insights from Project 1",
@@ -157,7 +157,7 @@ class ProjectPage:
             },
                         {
                 "title": "Income and BMI Prediction",
-                "image": "https://github.com/nitzmali/portfolio/blob/main/assets/images/BMI_output_2png.png?raw=true",
+                "image": "https://github.com/nitzmali/portfolio/blob/main/assets/images_resized/BMI_output_2png.png?raw=true",
                 "description": "Advancements in Predictive Modeling: Income and BMI Analysis",
                 "details":"In two significant projects, I demonstrated the power of predictive modeling, first by developing a model to predict household income and then by creating a model to estimate Body Mass Index (BMI). The household income project involved comprehensive steps such as data loading, feature engineering, model development and validation, and interpretation. Special attention was given to the unique challenge of predicting household income based on individual records, employing techniques like Neighbours Similarity Algorithm and Recursive Feature Elimination to ensure accuracy and quality.</p><p>The BMI prediction project was equally intricate, utilizing demographic data to predict BMI with a focus on both interpretability and performance. This project involved various statistical models and tests, such as OLS, Anova, and Tukey HSD, and employed different forms of linear regression, including Ridge, Lasso, and Elastic-Net, to provide a nuanced understanding of BMI factors. Both projects not only highlighted my skills in data science and statistical analysis but also underscored the importance of predictive modeling in extracting meaningful insights from complex datasets.",
                 "insights": "Insights from Project 1",
@@ -167,7 +167,53 @@ class ProjectPage:
                 "pdf":False,
                 "pdf_file_path":''
             },
-                        {
+            {
+                "title": "AI Powered Job Transition Pathway using Generative LSTM Models",
+                "image": "https://github.com/nitzmali/portfolio/blob/main/assets/images_resized/job_transition.png?raw=true",
+                "description": "This comprehensive project in Data Science delves into the dynamic nature of the modern workforce.",
+                "details":'''<div>
+                            <h2>FutureScape Navigator: Transforming Career Journeys with Generative AI and LSTM Insights</h2>
+                            <h3>Motivation</h3>
+                            <p>
+                                The modern workforce's dynamic nature calls for continuous adaptation in skills and knowledge. This project arose from a deep understanding of the challenges faced by professionals in plotting their career paths amidst rapidly changing job categories and skill requirements. By leveraging machine learning models, specifically seq2seq models with bidirectional LSTM and attention mechanisms, the project aims to offer tailored career guidance solutions, adapting to the fast-paced professional environment.
+                            </p>
+                            <h3>Background Information</h3>
+                            <p>
+                                The research underscores the trend of career switching, reflecting the changing dynamics of work, skill demands, and personal aspirations. It highlights the increasing trend of labor mobility across various age groups, emphasizing the need for tools that facilitate smooth career transitions and enable professionals to tailor their career paths.
+                            </p>
+                            <h3>Literature Review</h3>
+                            <p>
+                                The study juxtaposes its unique approach against existing literature, focusing on the enhancement of job-skill representation and the importance of understanding job transition patterns. It stands out for its emphasis on predicting educational courses and career progression using a novel AI approach.
+                            </p>
+                            <h3>Methodology</h3>
+                            <p>
+                                Comprehensive data collection from online job portals, skill repositories, and educational platforms form the foundation of this research. The methodology encompasses data cleaning, exploratory data analysis, and sophisticated data processing techniques, including the use of Phrase Matcher Models and fuzzy matching to refine and correlate data across different domains.
+                            </p>
+                            <h3>ML Modeling</h3>
+                            <p>
+                                The heart of the project lies in its advanced ML modeling, utilizing Seq2Seq architecture with LSTM layers. These models, coupled with BERT embeddings, are adept at capturing the nuances of language and context, crucial for accurately mapping career paths and skill development trajectories.
+                            </p>
+                            <h3>Experiments and Model Evaluation</h3>
+                            <p>
+                                The project delves into various experiments to fine-tune the model, exploring different embeddings and LSTM configurations. The evaluation metrics focus on the model's accuracy in forecasting job roles and the relevance of course recommendations, providing an objective assessment of its effectiveness.
+                            </p>
+                            <h3>Project Results</h3>
+                            <p>
+                                The project successfully demonstrates a model-generated output showcasing potential career pathways, requisite skills, and tailored course recommendations, thus offering a comprehensive tool for career planning and skill development.
+                            </p>
+                            <h3>Discussion and Future Improvement</h3>
+                            <p>
+                                The research concludes with a critical discussion on its holistic approach, potential improvements, and future directions. It emphasizes the need for continuous refinement of the model to keep pace with the evolving job market and skill landscape.
+                            </p>
+                        </div>''',
+                "insights": "Insights from Project 1",
+                "visualizations": "URL_to_visualization_1",  # or path to local file
+                "code": "https://github.com/nitzmali/job_transition_pathway",
+                "conclusion": "Conclusion of Project 1",
+                "pdf":False,
+                "pdf_file_path":''
+            },
+            {
                 "title": "Machine Learning Image Colorization",
                 "image": "https://github.com/nitzmali/portfolio/blob/main/assets/images_resized/color_to_gray.png?raw=true",
                 "description": "Reviving Colors through AI: Machine Learning Image Colorization",
@@ -188,7 +234,7 @@ class ProjectPage:
 
                 with tab1:
                     if not pdf:
-                        st.write(project["details"])
+                        st.markdown(project["details"], unsafe_allow_html=True)
                     else:
                         st.markdown(return_pdf_embed(project["pdf_file_path"]), unsafe_allow_html=True)
 
@@ -218,6 +264,32 @@ class ProjectPage:
                         st.markdown(f'<iframe src="{nbviewer_url}" width="100%" height="800" frameborder="0"></iframe>', unsafe_allow_html=True)
                         st.write("BMI -- Regression Machine Learning Model")
                         github_url = 'https://github.com/nitzmali/BMI_PREDICTION/blob/main/2.3%20-%20Encoding%20and%20Modelling%20%5BBMI%5D.ipynb?raw=true'
+                        nbviewer_url = github_url.replace('github.com', 'nbviewer.jupyter.org/github')
+                        st.markdown(f'<iframe src="{nbviewer_url}" width="100%" height="800" frameborder="0"></iframe>', unsafe_allow_html=True)
+
+                    if project["title"]=="AI Powered Job Transition Pathway using Generative LSTM Models":
+                        st.write("## Data Loading and Processing")
+                        github_url = 'https://github.com/nitzmali/job_transition_pathway/blob/main/notebooks/data_process.ipynb?raw=true'
+                        nbviewer_url = github_url.replace('github.com', 'nbviewer.jupyter.org/github')
+                        st.markdown(f'<iframe src="{nbviewer_url}" width="100%" height="800" frameborder="0"></iframe>', unsafe_allow_html=True)
+                        st.write("## Clustering Job Ttiles using Spacy")
+                        github_url = 'https://github.com/nitzmali/job_transition_pathway/blob/main/notebooks/build_clustering_titles_skills_iden_models.ipynb?raw=true'
+                        nbviewer_url = github_url.replace('github.com', 'nbviewer.jupyter.org/github')
+                        st.markdown(f'<iframe src="{nbviewer_url}" width="100%" height="800" frameborder="0"></iframe>', unsafe_allow_html=True)
+                        st.write("## Generate Synthetic data to map job transitions")
+                        github_url = 'https://github.com/nitzmali/job_transition_pathway/blob/main/notebooks/jobs_data_generation.ipynb?raw=true'
+                        nbviewer_url = github_url.replace('github.com', 'nbviewer.jupyter.org/github')
+                        st.markdown(f'<iframe src="{nbviewer_url}" width="100%" height="800" frameborder="0"></iframe>', unsafe_allow_html=True)
+                        st.write("## Job Prediction")
+                        github_url = 'https://github.com/nitzmali/job_transition_pathway/blob/main/notebooks/final_models/job_prediction.ipynb?raw=true'
+                        nbviewer_url = github_url.replace('github.com', 'nbviewer.jupyter.org/github')
+                        st.markdown(f'<iframe src="{nbviewer_url}" width="100%" height="800" frameborder="0"></iframe>', unsafe_allow_html=True)
+                        st.write("## Skills Prediction")
+                        github_url = 'https://github.com/nitzmali/job_transition_pathway/blob/main/notebooks/final_models/skills_prediction.ipynb?raw=true'
+                        nbviewer_url = github_url.replace('github.com', 'nbviewer.jupyter.org/github')
+                        st.markdown(f'<iframe src="{nbviewer_url}" width="100%" height="800" frameborder="0"></iframe>', unsafe_allow_html=True)
+                        st.write("## Course Prediction")
+                        github_url = 'https://github.com/nitzmali/job_transition_pathway/blob/main/notebooks/final_models/course_prediction.ipynb?raw=true'
                         nbviewer_url = github_url.replace('github.com', 'nbviewer.jupyter.org/github')
                         st.markdown(f'<iframe src="{nbviewer_url}" width="100%" height="800" frameborder="0"></iframe>', unsafe_allow_html=True)    
 
@@ -254,6 +326,19 @@ class ProjectPage:
                             </div>
                         """, unsafe_allow_html=True)
                         create_tabs(project,project["pdf"])
+
+                st.markdown("""
+                    <hr style="
+                        border: none; 
+                        height: 1px; 
+                        background: linear-gradient(to right, #B2DFDB 0%, #B2DFDB 100%); 
+                        margin-top: 0px; 
+                        margin-bottom: 0px;">
+                """, unsafe_allow_html=True)
+
+                #st.markdown('<hr style="border:2px solid #008080; width:50%; margin:auto;"/>', unsafe_allow_html=True)
+
+   
 
 
 
